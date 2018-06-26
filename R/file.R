b456aaf43730fab7ae6830078575d55cf7b7470e
hadley <h.wickham@gmail.com>
2018-06-26 13:43:27 -0500

    Extract requirements from Rmd files

--- R/file.R
+++ R/file.R
@@ -1,10 +1,19 @@
 #' Extract requirements from a file
 #'
+#' @description
+#' * `.R`: extracts requirements from parsed code
+#'
+#' * `.Rmd`: requirements from chunks. If rmarkdown package is installed,
+#'   will also add requirements from custom output type.
+#'
 #' @param path Path to file
 #' @export
 #' @examples
-#' path <- system.file("examples", "simple.R", package = "requirements")
-#' req_file(path)
+#' path_r <- system.file("examples", "simple.R", package = "requirements")
+#' path_rmd <- system.file("examples", "simple.Rmd", package = "requirements")
+#'
+#' req_file(path_r)
+#' req_file(path_rmd)
 req_file <- function(path) {
   if (!file.exists(path)) {
     stop("`path` does not exist", call. = FALSE)
@@ -13,15 +22,66 @@ req_file <- function(path) {
 
   switch(ext,
     r = req_file_r(path),
+    rmd = req_file_rmd(path),
     character()
   )
 }
+
+# .R ----------------------------------------------------------------------
 
 req_file_r <- function(path) {
   tryCatch(
     error = function(err) character(),
     {
       code <- parse(path)
+      req_code(!!code)
+    }
+  )
+}
+
+# .Rmd --------------------------------------------------------------------
+
+req_file_rmd <- function(path) {
+  lines <- readLines(path)
+
+  chunks <- rmd_chunks(lines)
+  chunk_reqs <- flat_map_chr(chunks, req_text)
+
+  if (!requireNamespace("rmarkdown", quietly = TRUE)) {
+    yaml_reqs <- character()
+  } else {
+    format <- rmarkdown::default_output_format(path)
+    yaml_reqs <- req_text(format)
+  }
+
+  reqs <- c("rmarkdown", chunk_reqs, yaml_reqs)
+  unique(reqs)
+}
+
+rmd_chunks <- function(lines) {
+  # From https://github.com/rstudio/rstudio/blob/0edb05f67b4f2eea25b8cfb15f7c64ec9b27b288/src/gwt/acesupport/acemode/rmarkdown_highlight_rules.js#L181-L184
+  chunk_start_re <- "^(?:[ ]{4})?`{3,}\\s*\\{[Rr]\\b(?:.*)engine\\s*\\=\\s*['\"][rR]['\"](?:.*)\\}\\s*$|^(?:[ ]{4})?`{3,}\\s*\\{[rR]\\b(?:.*)\\}\\s*$";
+  chunk_end_re <- "^(?:[ ]{4})?`{3,}\\s*$"
+
+  chunk_start <- grepl(chunk_start_re, lines, perl = TRUE)
+  chunk_end <- grepl(chunk_end_re, lines, perl = TRUE)
+
+  chunk_num <- cumsum(chunk_start)
+  in_chunk <- (chunk_num - cumsum(chunk_end)) != 0
+
+  chunks <- split(lines[in_chunk], chunk_num[in_chunk])
+  names(chunks) <- NULL
+
+  # Strip off first element, the chunk header
+  chunks <- lapply(chunks, function(x) x[-1])
+  lapply(chunks, paste, collapse = "\n")
+}
+
+req_text <- function(text) {
+  tryCatch(
+    error = function(err) character(),
+    {
+      code <- parse(text = text)
       req_code(!!code)
     }
   )
--- tests/testthat/test-file.R
+++ tests/testthat/test-file.R
@@ -1,5 +1,8 @@
 context("test-file.R")
 
+
+# .R ----------------------------------------------------------------------
+
 test_that("can extract requirements from .R file", {
   expect_equal(req_file(test_path("file-ok.R")), c("x", "y", "z"))
 })
@@ -10,4 +13,23 @@ test_that("unparseable file requires nothing ", {
 
 test_that("fails if path does not exist", {
   expect_error(req_file("DNE.r"), "does not exist")
+})
+
+# .Rmd --------------------------------------------------------------------
+
+test_that("inspects output_format", {
+  skip_if_not_installed("rmarkdown")
+
+  expect_equal(
+    req_file(test_path("file-format.Rmd")),
+    c("rmarkdown", "tools")
+  )
+
+})
+
+test_that("unparsed chunks are ignored", {
+  expect_equal(
+    req_file(test_path("file-partially-broken.Rmd")),
+    c("rmarkdown", "x")
+  )
 })
